3e720f6b5669edb22ae09fd9f9f31b7c0b87078f
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2021-04-12 22:07:45 +0200

    sistemato error handler

--- root/manager/error.py
+++ root/manager/error.py
@@ -6,7 +6,7 @@ import sys
 import traceback
 from telegram import Update
 from telegram.ext import CallbackContext
-from root.util.util import retrieve_key
+from root.util.util import format_error, retrieve_key
 import root.util.logger as logger
 from root.contants.messages import TELEGRAM_ERROR, USER_ERROR
 from root.util.telegram import TelegramSender
@@ -23,19 +23,10 @@ def handle_error(update: Update, context: CallbackContext):
         context (CallbackContext): The context of the telegram bot
     """
     if update:
-        chat_id = retrieve_key("ERROR_CHANNEL")
+        error_channel = retrieve_key("ERROR_CHANNEL")
         if update.effective_message:
-            sender.send_and_delete(
-                update.effective_message.message_id,
-                update.effective_message.from_user.id,
-                context,
-                chat_id,
-                USER_ERROR,
-                reply_to_message_id=update.effective_message.message_id,
-                timeout=LONG_SERVICE_TIMEOUT,
-            )
-            update.effective_message.reply_text(USER_ERROR)
-        trace = "".join(traceback.format_tb(sys.exc_info()[2]))
-        logger.error(trace)
-        text = TELEGRAM_ERROR % context.error
-        context.bot.send_message(chat_id, text, parse_mode="HTML")
+            if update.effective_chat.id != error_channel:
+                update.effective_message.reply_text(USER_ERROR)
+        logger.error(format_error)
+        text = TELEGRAM_ERROR % format_error(context.error)
+        context.bot.send_message(error_channel, text, parse_mode="HTML")
