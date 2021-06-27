5a811ecf21f9d1ee66de503d73cac42dd88d90bc
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2021-06-27 22:29:45 +0200

    altro messaggio editato

--- root/contants/messages.py
+++ root/contants/messages.py
@@ -825,8 +825,8 @@ EDIT_LINK_TO_WISHLIST_ITEM_MESSAGE = (
 
 SUPPORTED_LINKS_MESSAGE = (
     "🆕  Da oggi i seguenti siti supportano il download automatico delle foto:\n\n"
-    "   •  GameStop.it;\n"
-    "   •  Multiplayer.com.\n\n"
+    "   •  GameStop.it ;\n"
+    "   •  Multiplayer.com .\n\n"
     "Aggiungi ora un prodotto e lascia che sia io a occuparmi delle scartoffie!"
 )
 
