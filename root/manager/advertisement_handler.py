0c87e3379051a8882e28764c7b02293748f4ddca
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2021-08-23 22:47:24 +0200

    modificato durata degli ads a 30 minuti

--- root/manager/advertisement_handler.py
+++ root/manager/advertisement_handler.py
@@ -3,7 +3,7 @@
 
 from root.contants.keyboard import ADS_KEYBOARDS
 from telegram.update import Update
-from root.contants.message_timeout import FIFTEEN_MINUTES
+from root.contants.message_timeout import FIFTEEN_MINUTES, THIRTY_MINUTES
 from telegram.message import Message
 from root.contants.messages import ADS_MESSAGES
 from telegram.ext.callbackcontext import CallbackContext
@@ -18,7 +18,7 @@ def send_advertisement(context: CallbackContext, group_id: int = None):
         group_id: int = args["group"]
     message = random.choice(ADS_MESSAGES)
     keyboard = ADS_KEYBOARDS[ADS_MESSAGES.index(message)]
-    send_and_delete(group_id, message, reply_markup=keyboard, timeout=FIFTEEN_MINUTES)
+    send_and_delete(group_id, message, reply_markup=keyboard, timeout=THIRTY_MINUTES)
 
 
 def command_send_advertisement(update: Update, context: CallbackContext):
